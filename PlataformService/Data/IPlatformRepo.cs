73e981a56f146163fde5828d72128902a926c6aa
Gabriel Pontes <gabriel.sommelier@outlook.com>
2024-05-02 19:49:30 -0300

    Criação de Repositotrio e inteface

---
+++ PlataformService/Data/IPlatformRepo.cs
@@ -0,0 +1,15 @@
+﻿using PlataformService.Models;
+
+namespace PlataformService.Data
+{
+    public interface IPlatformRepo
+    {
+        bool SaveChanges();
+
+        IEnumerable<Platform> GetAllPlatforms();
+
+        Platform GetPlatformById(int id);
+
+        void CreatePlatForm(Platform plat);
+    }
+}
