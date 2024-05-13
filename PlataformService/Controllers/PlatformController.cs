cc8a84a0a1698e1a0f8ec48729b62f9d855921d4
Gabriel Pontes <gabriel.sommelier@outlook.com>
2024-05-13 20:18:41 -0300

    Finalização do controller de Platform e criação do dockerfile e sua imagem

--- PlataformService/Controllers/PlatformController.cs
+++ PlataformService/Controllers/PlatformController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using PlataformService.Data;
 using PlataformService.Dtos;
+using PlataformService.Models;
 
 namespace PlataformService.Controllers
 {
@@ -26,6 +27,35 @@ namespace PlataformService.Controllers
             var platformItens = _repository.GetAllPlatforms();
 
             return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItens));
+        }
+
+        [HttpGet("{id}", Name = "GetPlatformById")]
+        public ActionResult<PlatformReadDto> GetPlatformById(int id)
+        {
+            var platformItem = _repository.GetPlatformById(id);
+
+            if (platformItem != null) 
+            {
+            
+                return Ok(_mapper.Map<PlatformReadDto>(platformItem));
+            
+            }
+
+            return NotFound();
+        }
+        [HttpPost]
+        public ActionResult<PlatformReadDto> CreatePlatform(PlatformCreatDto platformCreatDto)
+        {
+            var platformModel = _mapper.Map<Platform>(platformCreatDto);
+
+            _repository.CreatePlatForm(platformModel);
+            _repository.SaveChanges();
+
+            var platformReadDto = _mapper.Map<PlatformReadDto>(platformModel);
+
+            return CreatedAtRoute(nameof(GetPlatformById), new { Id = platformReadDto.Id }, platformReadDto);
+
+
         }
 
     }
